cebc4e76c9acc5d89a672738a71542e20974eef0
fourarmsrk-max <fourarmsrk@gmail.com>
2026-07-16 04:01:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8940097553:AAHHeaxRwj4TTJwHUjZZsd4AMoiOWPjiZ_0")
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN environment variable not found.")
